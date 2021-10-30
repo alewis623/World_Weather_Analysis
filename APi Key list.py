eea68b9eacd34d649badd0db4cc53cc8eafe6050
Andrew Lewis <andrewlewisaboc@yahoo.com>
2021-10-30 12:53:42 -0500

    adding weather py only

--- APi Key list.py
+++ APi Key list.py
@@ -1,10 +1,10 @@
-Wall Street Journal
-api_key ="e13f50b3e3db4a00bbc463cb63ef5873"
-New York Times
-api_key = "wPLrrQ4iKTsq1xXxNKlG1ZMbIMOvvKEQ"
-OpenWeather
-api_key = "a06a4fc51dd66de1e2b3c6c775b03075"
-OMDb
-api_key = "ff77d42b"
-Google
+#Wall Street Journal
+wsj_key ="e13f50b3e3db4a00bbc463cb63ef5873"
+#New York Times
+nyt_key = "wPLrrQ4iKTsq1xXxNKlG1ZMbIMOvvKEQ"
+#OpenWeather
+weather_api_key = "a06a4fc51dd66de1e2b3c6c775b03075"
+#OMDb
+omdb_key = "ff77d42b"
+#Google
 g_key = "AIzaSyB2eWDl-AxWbstDGvLuAKyuPJwOYtOAC98"
--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-api_key="wPLrrQ4iKTsq1xXxNKlG1ZMbIMOvvKEQ"
+#Wall Street Journal
+wsj_key ="e13f50b3e3db4a00bbc463cb63ef5873"
+#New York Times
+nyt_key = "wPLrrQ4iKTsq1xXxNKlG1ZMbIMOvvKEQ"
+#OpenWeather
+weather_api_key = "a06a4fc51dd66de1e2b3c6c775b03075"
+#OMDb
+omdb_key = "ff77d42b"
+#Google
+g_key = "AIzaSyB2eWDl-AxWbstDGvLuAKyuPJwOYtOAC98"
