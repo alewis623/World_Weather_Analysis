5903627983a9ca11297b7e35b347361b388ded68
Andrew Lewis <andrewlewisaboc@yahoo.com>
2021-10-31 09:33:55 -0500

    Weatherpy with googleapi

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ weather_api_key = "a06a4fc51dd66de1e2b3c6c775b03075"
 #OMDb
 omdb_key = "ff77d42b"
 #Google
-g_key = "AIzaSyB2eWDl-AxWbstDGvLuAKyuPJwOYtOAC98"
+g_key = "AIzaSyAMdD9osh0w3qvhQ445is2o9I8R-UCIRBQ"
